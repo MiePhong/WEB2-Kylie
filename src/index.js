ef416a434cf714d9a4644ad5b1e10512665f1acb
Tam Nguyen <ngthithutam910@gmail.com>
2023-03-08 23:01:09 +0700

    mọi người pull bài này về làm trang web theo video gửi trong nhóm zalo làm đến bài 8 trong dach sách phát còn mấy cái html và css bên ngoài là cái mà mọi người thiết kế

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// const express = require('express')
+import express from 'express';
+import  configViewEngine  from './configs/viewEngine';
+import initWebRoute from './route/web';
+
+const app = express()
+const port = 8080
+
+//set up view engine
+configViewEngine(app);
+
+// init web route
+initWebRoute(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
